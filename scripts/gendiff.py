fca12fb7adcdcd6a536b5225f94a760eb65a9399
yng-trouble <nippefour@gmail.com>
2026-01-22 18:19:00 +0400

    Add script with manual

---
+++ scripts/gendiff.py
@@ -0,0 +1,14 @@
+import argparse
+
+def run_gendiff():
+    parser = argparse.ArgumentParser(
+        prog='gendiff',
+        description='''Compares two configuration files and shows a difference.''')
+    
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    args = parser.parse_args()
+
+if __name__ == '__main__':
+    run_gendiff()
